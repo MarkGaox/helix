b9f8a4b02191939ea0c393810ab201cba95fcb1f
Xiaxuan Gao <xiaxgao@linkedin.com>
2023-08-23 12:02:55 -0700

    b

--- helix-rest/src/test/java/org/apache/helix/rest/server/TestClusterAccessor.java
+++ helix-rest/src/test/java/org/apache/helix/rest/server/TestClusterAccessor.java
@@ -1415,7 +1415,6 @@ public class TestClusterAccessor extends AbstractTestClass {
     System.out.println("End test :" + TestHelper.getTestMethodName());
   }
 
-
   @Test
   public void testClusterFreezeMode() throws Exception {
     String cluster = "TestCluster_0";

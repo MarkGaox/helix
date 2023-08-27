5e1ff2f22748b76915a4a4851d458bca5c28b2bd
Xiaxuan Gao <xiaxgao@linkedin.com>
2023-08-27 15:11:32 -0700

    Add timeout to let previous message expire

--- helix-core/src/test/java/org/apache/helix/integration/messaging/TestP2PNoDuplicatedMessage.java
+++ helix-core/src/test/java/org/apache/helix/integration/messaging/TestP2PNoDuplicatedMessage.java
@@ -191,6 +191,8 @@ public class TestP2PNoDuplicatedMessage extends ZkTestBase {
           "Number of successful p2p transitions when disable instance " + ins + ": " + p2pTriggered
               + " , expect: " + total);
 
+      Thread.sleep(5000);
+
       System.out.println("********************* Enable instance: " + ins);
       _gSetupTool.getClusterManagementTool().enableInstance(CLUSTER_NAME, ins, true);
       Assert.assertTrue(_clusterVerifier.verifyByPolling());

53326fe7d0811fd59d0b3f771d9b12a54ef4efdc
Xiaxuan Gao <xiaxgao@linkedin.com>
2023-08-29 00:37:19 -0700

    remove sleep

--- helix-core/src/test/java/org/apache/helix/integration/messaging/TestP2PNoDuplicatedMessage.java
+++ helix-core/src/test/java/org/apache/helix/integration/messaging/TestP2PNoDuplicatedMessage.java
@@ -129,6 +129,7 @@ public class TestP2PNoDuplicatedMessage extends ZkTestBase {
     _configAccessor = new ConfigAccessor(_gZkClient);
     _accessor = new ZKHelixDataAccessor(CLUSTER_NAME, _baseAccessor);
 
+
     for (int i = 0; i < DB_COUNT; i++) {
       String testDB = "TestDB_" + i;
       StateTransitionTimeoutConfig stateTransitionTimeoutConfig =
